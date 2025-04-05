80ef178c73248da49f188d873f62f09f0dd7bfc0
f95f <95felipefernandes@gmail.com>
2025-04-05 05:24:03 -0300

    [feat] adds link to message page.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,14 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared/shared.module';
 import { UserListComponent } from './user-list/user-list.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     UserListComponent,
+    ChatListComponent,
   ],
   imports: [
     PagesRoutingModule,
